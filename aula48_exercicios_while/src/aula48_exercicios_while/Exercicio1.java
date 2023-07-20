5da137bc56da6d6fa18582034106b8b632d13a74
feli0x1 <sdiylaogzbhk@proton.me>
2023-07-20 17:20:39 -0300

    Exercício 1 da aula 48 resolvido

---
+++ aula48_exercicios_while/src/aula48_exercicios_while/Exercicio1.java
@@ -0,0 +1,19 @@
+package aula48_exercicios_while;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("Senha Invalida");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+	}
+}
