8b764405a1798e21681bedf33d4e2e187d3622ae
feli0x1 <sdiylaogzbhk@proton.me>
2023-07-20 17:31:28 -0300

    Exercício 2 da aula 48 resolvido

---
+++ aula48_exercicios_while/src/aula48_exercicios_while/Exercicio2.java
@@ -0,0 +1,33 @@
+package aula48_exercicios_while;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x, y;
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		while (x != 0 && y != 0) {
+			if (x > 0 && y > 0) {
+				System.out.println("primeiro");
+			}
+			else if (x < 0 && y > 0) {
+				System.out.println("segundo");
+			}
+			else if (x < 0 && y < 0) {
+				System.out.println("terceiro");
+			}
+			else {
+				System.out.println("quarto");
+			}
+			x = sc.nextInt();
+			y = sc.nextInt();
+		}
+		sc.close();
+	}
+
+}
