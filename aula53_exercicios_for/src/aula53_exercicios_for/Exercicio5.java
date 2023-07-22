b8c502752d6b24cb3de1f063e40368b3460b8916
feli0x1 <sdiylaogzbhk@proton.me>
2023-07-21 23:09:07 -0300

    Exercício 5 da aula 53 resolvido

---
+++ aula53_exercicios_for/src/aula53_exercicios_for/Exercicio5.java
@@ -0,0 +1,18 @@
+package aula53_exercicios_for;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int fatorial = 1;
+		
+		for (int i = 1; i < n; i++) {
+			fatorial = fatorial * (i + 1);
+		}
+		
+		System.out.println(fatorial);
+		sc.close();
+	}
+}
