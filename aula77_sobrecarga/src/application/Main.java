bc2a9c4fe6ffb0ec44e11354bb9bdb474181df9b
feli0x1 <sdiylaogzbhk@proton.me>
2023-07-24 12:53:34 -0300

    Conclusão da aula 77

---
+++ aula77_sobrecarga/src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Product;
+
+public class Main {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter product data: "); 
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Price: ");
+		double price = sc.nextDouble();
+				
+		Product p = new Product(name, price);
+		
+		System.out.println();
+		System.out.println("Product data: " + p);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		p.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + p);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		p.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + p);
+		sc.close();
+	}
+}
