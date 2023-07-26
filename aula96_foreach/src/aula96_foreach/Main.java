b013381cd40a808a73e7f118c12ea307dea1984c
feli0x1 <sdiylaogzbhk@proton.me>
2023-07-26 17:08:03 -0300

    Conclusão da aula 96

---
+++ aula96_foreach/src/aula96_foreach/Main.java
@@ -0,0 +1,12 @@
+package aula96_foreach;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] numbers = new int[] {1, 2, 3, 4, 5};
+		
+		for (int number : numbers) {
+			System.out.println(number);
+		}
+	}
+}
