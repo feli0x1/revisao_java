7901d7d40ce82bd90cffff4019830ab6ed0b599f
feli0x1 <sdiylaogzbhk@proton.me>
2023-08-09 18:45:36 -0300

    Conclusão da aula 226

---
+++ aula226_solucao_problema_sem_interface/src/model/services/RentalService.java
@@ -0,0 +1,35 @@
+package model.services;
+
+import java.time.Duration;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	private Double pricePerHour;
+	private Double pricePerDay;
+	private BrazilTaxService taxService;
+	
+	public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60.0;
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24.0) * pricePerDay; 
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
