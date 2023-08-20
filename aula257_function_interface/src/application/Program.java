7a00d4a66127e51a8ff27c00b455e4a8b1157cbc
feli0x1 <sdiylaogzbhk@proton.me>
2023-08-20 13:01:47 -0300

    Expressão lambda declarada

--- aula257_function_interface/src/application/Program.java
+++ aula257_function_interface/src/application/Program.java
@@ -3,10 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import model.entities.Product;
-import model.util.UpperCaseName;
 
 public class Program {
 	public static void main(String[] args) {
@@ -18,8 +18,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		Function<Product, String> func = p -> p.getName().toUpperCase();
+		
 		List<String> productNames = list.stream()
-				.map(Product::nonStaticUpperCaseName)
+				.map(func)
 				.collect(Collectors.toList());
 		
 		productNames.forEach(System.out::println);
